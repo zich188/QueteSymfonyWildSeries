b4124f109ada7c6f6ad0b7a25bf8b24b5452fb45
zich188 <78915934+zich188@users.noreply.github.com>
2021-05-22 12:06:37 +0200

    Routing avancée

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ProgramController
+ * @Route ("/programs", name="program_")
+ */
 Class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * Correspond à la route /programs/ et au name "program_index"
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', ['website' => 'Wild Séries',]);
+    }
+
+    /**
+     * Correspond à la route /programs/{id} et au name "program_show"
+     * @Route("/{id}/", methods={"GET"}, requirements={"id"="\d+"}, name="show")
+     */
+    public function show(string $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
     }
 }
