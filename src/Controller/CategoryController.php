1f9427158de4e38472a394b8d5b6f426d30ae323
zich188 <78915934+zich188@users.noreply.github.com>
2021-05-31 16:22:06 +0200

    12SymfonyGardezLaForm

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,9 +6,13 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 
 /**
  * Class CategoryController
@@ -27,6 +31,38 @@ class CategoryController extends AbstractController
             ->getRepository(Category::class)
             ->findAll();
         return $this->render('category/index.html.twig', ['categories' => $categories]);
+    }
+
+    /**
+     * The controller for the category add form
+     * Correspond à la route /categories/new et au name "category_new"
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        // Create a new Category Object
+        $category = new Category();
+        // Create the associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Render the form
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // For example : persiste & flush the entity
+            // And redirect to a route that display the result
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($category);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
     }
 
     /**
